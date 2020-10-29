9e2bced9c0fa27b618b3d17adbb63823bce249de
Stephen Stubbs <stephenstubbsbeta@gmail.com>
2020-10-29 16:12:57 +0000

    Implemented rotating sphere of particles

--- particle-orb.js
+++ particle-orb.js
@@ -1,5 +1,17 @@
-//Initialise particles container canvas
-const particlesContainer = document.getElementById("particle-container");
-const ctx = particlesContainer.getContext("2d");
-particlesContainer.width = window.innerWidth;
-particlesContainer.height = window.innerHeight;
+//Global constants
+const particleContainer = document.getElementById("particle-container");
+const numParticles = 250;
+const orbRadius = "200px";
+
+//Create each particle
+for(let i = 0; i < numParticles; i++) {
+    //Create particle div
+    let particle = document.createElement("div");
+    particle.classList.add("particle");
+    //Set random angle of rotation between 0 and 360
+    let angY = Math.floor(Math.random() * 360);
+    let angZ = Math.floor(Math.random() * 360);
+    //Add particle to DOM and apply transformations
+    particleContainer.appendChild(particle);
+    particle.style.transform = `rotateY(${angY}deg) rotateZ(${angZ}deg) translateX(${orbRadius})`;
+}
