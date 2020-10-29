a18ed9285fa47272004accf5ef4a9cb1a69b1425
Stephen Stubbs <stephenstubbsbeta@gmail.com>
2020-10-29 11:40:44 +0000

    Initial commit

---
+++ particle-orb.js
@@ -0,0 +1,5 @@
+//Initialise particles container canvas
+const particlesContainer = document.getElementById("particle-container");
+const ctx = particlesContainer.getContext("2d");
+particlesContainer.width = window.innerWidth;
+particlesContainer.height = window.innerHeight;
